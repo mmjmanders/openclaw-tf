3f4071a0a1bfe76b5a99046ab4948c3026141817
Mark Manders <mmjmanders@gmail.com>
2026-03-13 12:57:27 +0100

    Add region configuration to SST and provision Lightsail instance resources
    - Configure `aws` provider in SST with `eu-central-1` region.
    - Add Lightsail instance with public ports and secrets for key pair and CIDR restrictions.

--- infra/lightsail.ts
+++ infra/lightsail.ts
@@ -1,6 +1,31 @@
-const publicKey = new sst.Secret("LightsailPublicKey", "public-key")
+const {LIGHTSAIL_BUNDLE_ID: bundleId, AWS_DEFAULT_REGION: region} = process.env
 
-export const lightsailKeyPar = new aws.lightsail.KeyPair("KeyPair", {
+const publicKey = new sst.Secret("LightsailPublicKey", "public-key")
+const allowedCidrs = new sst.Secret("LightsailAllowedCidrs", "allowed-cidrs")
+
+const lightsailKeyPar = new aws.lightsail.KeyPair("KeyPair", {
     name: `${$app.stage}-keypair`,
     publicKey: publicKey.value,
+})
+
+export const lightsailInstance = new aws.lightsail.Instance("Instance", {
+    name: `${$app.stage}-lightsail-instance`,
+    blueprintId: 'openclaw_ls_1_0',
+    bundleId,
+    availabilityZone: `${region}a`,
+})
+
+const lightsailPublicPorts = new aws.lightsail.InstancePublicPorts("PublicPorts", {
+    instanceName: lightsailInstance.name,
+    portInfos: [{
+        fromPort: 22,
+        toPort: 22,
+        protocol: 'tcp',
+        cidrs: allowedCidrs.value.apply(t => t.split(/,\s+/))
+    }, {
+        fromPort: 443,
+        toPort: 443,
+        protocol: 'tcp',
+        cidrs: allowedCidrs.value.apply(t => t.split(/,\s+/))
+    }]
 })
--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,20 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "openclaw-aws",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-    };
-  },
-  async run() {
-    await import('./infra/lightsail');
-  },
+    app(input) {
+        return {
+            name: "openclaw-aws",
+            removal: input?.stage === "production" ? "retain" : "remove",
+            protect: ["production"].includes(input?.stage),
+            home: "aws",
+            providers: {
+                aws: {
+                    region: 'eu-central-1'
+                }
+            }
+        };
+    },
+    async run() {
+        await import('./infra/lightsail');
+    },
 });
